65e7270cea0a0d8f8f2b103961558ffb4b7f3aa5
aslamkam <crispevening@gmail.com>
2025-09-16 14:16:36 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Section title="Kamal Aslam" className="hero">
         <div className="hero-intro">
           <p className="hero-subtitle">Hello :)</p>
-          <h1 className="hero-title">6+ years of experience in software development and system engineering, with expertise in algorithm design and optimization. Currently pursuing a Master's in Mathematics with a focus on Artificial Intelligence (AI). Skilled in machine learning, statistical modeling, and data analysis for complex, real-world datasets. Proficient in Python, C++, Java, and cloud technologies (AWS). Experienced in developing scalable systems and automating processes to improve efficiency. Strong background in mathematics (probability, optimization, linear algebra) applied to AI and machine learning challenges.</h1>
+          <h1 className="hero-title">Software Engineer & AI Specialist</h1>
           <a href="https://www.linkedin.com/in/kamal-aslam" className="hero-button">See my LinkedIn</a>
         </div>
       </Section>
